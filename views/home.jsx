1119303315e3617610e5750f8bfe3c5106854bc7
Brenton Ring <brentonring@Brentons-Air.lan>
2022-05-09 21:18:33 -0400

    Adding styles to REST-rant

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,7 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <button className='btn-primary'>Places Page</button>
             </main>
         </Def>
     )
