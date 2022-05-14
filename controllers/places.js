b7d86e99664565d52b43d8e44170f96c33e8ab44
Brenton Ring <brentonring@Brentons-Air.lan>
2022-05-14 14:02:24 -0400

    Added edit and delete functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,14 +1,32 @@
 const router = require('express').Router()
 const places = require('../models/places') // Could need to add .js
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
+// INDEX
 router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
 
+// NEW
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+// DELETE
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.status(303).redirect('/places')
+    }
+})
+
+// CREATE
 router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
@@ -25,6 +43,7 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })  
 
+// GET EDIT FORM
 router.get('/:id/edit', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -34,10 +53,39 @@ router.get('/:id/edit', (req, res) => {
         res.render('error404')
     }
     else {
-    res.render('places/edit', {place: places[id], id})
+        res.render('places/edit', {place: places[id], id})
     }
 })
 
+// PUT EDITS
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+})
+
+// SHOW
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,31 +1,33 @@
 const React = require('react')
 const Def = require('../default')
 
-function Edit ({place, index}) {
+function edit_form (data) {
     return (
         <Def>
             <main>
-                <h1>Edit a Place</h1>
-                <form method='POST' action={`/places/${index}?_method=PUT`}>
+                <h1>Edit Place</h1>
+                <form method='POST' action={`/places/${data.id}?_method=PUT`}>
                     <div className='form-group'>
                         <label htmlFor='name'>Place Name</label>
-                        <input defaultValue={place.name} className='form-control' required id='name' name='name'/>
+                        <input defaultValue={data.place.name} className='form-control' required id='name' name='name'/>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='pic'>Place Picture</label>
-                        <input defaultValue={place.pic} className='form-control' type='url' id='pic' name='pic'/>
+                        <input defaultValue={data.place.pic} className='form-control' id='pic' name='pic'/>
                     </div>
-                    <div className='form-group'>
-                        <label htmlFor='city'>City</label>
-                        <input defaultValue={place.city} className='form-control' id='city' name='city'/>
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='state'>State</label>
-                        <input defaultValue={place.state} className='form-control' id='state' name='state'/>
+                    <div className='row'>
+                        <div className='form-group col-sm-6'>
+                            <label htmlFor='city'>City</label>
+                            <input defaultValue={data.place.city} className='form-control' id='city' name='city'/>
+                        </div>
+                        <div className='form-group col-sm-6'>
+                            <label htmlFor='state'>State</label>
+                            <input defaultValue={data.place.state} className='form-control' id='state' name='state'/>
+                        </div>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='cuisines'>Cuisines</label>
-                        <input defaultValue={place.cuisines} className='form-control' required id='cuisines' name='cuisines'/>
+                        <input defaultValue={data.place.cuisines} className='form-control' required id='cuisines' name='cuisines'/>
                     </div>
                     <input className='btn btn-primary' type='submit' value='Edit Place'/>
                 </form>
@@ -34,4 +36,4 @@ function Edit ({place, index}) {
     )
 }
 
-module.exports = Edit
+module.exports = edit_form
