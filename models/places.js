074375ea56e0272ed874712072d260176d7e16a2
Brenton Ring <brentonring@Brentons-Air.lan>
2022-05-25 17:32:00 -0400

    added seed comments

--- models/places.js
+++ models/places.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const comment = require('./comment')
 
 const placeSchema = new mongoose.Schema({
     name: {type: String, required: true},
@@ -10,7 +11,8 @@ const placeSchema = new mongoose.Schema({
         type: Number,
         min: [1673, 'Surely not that old?!'],
         max: [new Date().getFullYear(), 'Hey, this year is in the future!']
-    }
+    },
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 })
 
 placeSchema.methods.showEstablished = function() {
