894fc8fe33c09eef469f5da7dcd10723bd7d6476
Brenton Ring <brentonring@Brentons-Air.lan>
2022-05-25 22:12:16 -0400

    added seed comments to show page

--- controllers/places.js
+++ controllers/places.js
@@ -48,7 +48,9 @@ router.put('/:id', (req, res) => {
 // SHOW
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+        console.log(place.comments)
         res.render('places/show', {place})
     })
     .catch(err => {
