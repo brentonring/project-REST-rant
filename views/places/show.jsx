1ed338faeaf9167e06327ee2b30c696d595feca9
Brenton Ring <brentonring@Brentons-Air.lan>
2022-05-27 23:19:12 -0400

    Implemented average rating system

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,7 +8,21 @@ function show (data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className='inactive'>
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className='border'>
@@ -42,7 +56,7 @@ function show (data) {
                 </div>
                 <div className='container'>
                     <h2>Rating</h2>
-                    <p>Not rated</p>
+                    {rating}
                 </div>
                 <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
                     Edit
